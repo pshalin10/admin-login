b71f4113c1518170388f661c0d2fb6a466378570
vishu9656 <31932564+vishu9656@users.noreply.github.com>
2017-11-09 11:07:29 -0500

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,15 +1,12 @@
 <?php
-$servername = "imc.kean.edu";
-$dbname = "seds2017";
+$servername ="imc.kean.edu";
 $username = "seds2017";
 $password = "2017fall";
-
-// Create connection
-$conn = new mysqli($servername, $dbname, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
+$dbname = "seds2017db";
+//create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname );
+//check connection
+if (!$conn){
+	die("Connection Failed: " . mysqli_connect_error());
+}
 ?>
