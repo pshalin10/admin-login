0907cf82f31f9ca4c1fe2cdca13357b64084cdd1
vishu9656 <31932564+vishu9656@users.noreply.github.com>
2017-11-09 12:13:51 -0500

    Update login_handle.php

--- login_handle.php
+++ login_handle.php
@@ -1,27 +1,51 @@
-<?php include 'dbconfig.php';?>
-<html>
-<body>
-<form method="post" action="success.php">
-  <div class="imgcontainer">
-  <title>Kean Career Services</title>
-  <center>
-    <img src="keanlogo.png" alt="Avatar" class="avatar" width="100" height="100">
-	<body background="background.jpg">
-  </div>
-  <center>
-  <div class="container">
-  <p>
-    <label><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required></p>
+<?php 
 
-    <label><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
-     <p>
-    <button type="submit">Login</button></a></p>
-    <input type="checkbox" checked="checked"> Remember me
-    <p><span class="psw">Forgot <a href="#">password?</a></span></p>
-  </div>
-  </center>
-  </body>
-</form>
-</html>
+	$page_title = 'Kean Career Services';
+	include ('includes/header.html');
+	include ('includes/db_config.php');
+
+	// Add the POST data to initial cookies.
+	error_reporting(E_ALL | E_STRICT);
+
+	// Verify user.
+	$query = sprintf("SELECT * FROM Consultants WHERE UPPER(username) = '%s'", strtoupper($_POST['login_username']));
+	$result = mysqli_query($conex, $query);
+
+	if ($result) {
+		if (mysqli_num_rows($result) > 0) {
+			// Verify username and password.
+			$query = sprintf("SELECT id, username, password FROM Consultants WHERE UPPER(username) = '%s' AND password = '%s'", strtoupper($_POST['login_username']), $_POST['login_password']);
+			$result = mysqli_query($conex, $query);
+
+			if ($result) {
+				if (mysqli_num_rows($result) > 0) {
+					$row = mysqli_fetch_array($result);
+					session_start();
+					$_SESSION["user_id"] = $row[0];
+					setcookie("logged_in", TRUE, time() + 86400); // Logged in
+					header("Location: staff.php");
+				} else {
+					echo "<p class='error'>User \"" . $_POST['login_username'] . "\" exists but password does not match.</p>";
+					echo "<br><a href='staff.php'>TRY AGAIN</a>";
+				}
+			} else {
+				echo "<p class='error'>Problem trying to login: " . mysqli_error();
+				echo "<br>Contact Administrator!</p>";
+				echo "<br><a href='staff.php'>TRY AGAIN</a>";
+			}
+		} else {
+			echo "<p class='error'>User \"" . $_POST['login_username'] . "\" does not exist in the database.</p>";
+			echo "<br><a href='staff.php'>TRY AGAIN</a>";
+		}
+	} else {
+		echo "<p class='error'>Problem trying to login: " . mysqli_error();
+		echo "<br>Contact Administrator!</p>";
+		echo "<br><a href='staff.php'>TRY AGAIN</a>";
+	}
+
+	// Free resources.
+	mysqli_free_result($result);
+	mysqli_close($conex);
+
+	include ('includes/footer.html');
+?>
