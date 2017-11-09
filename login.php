15699147207d6a1293f660f11091d9ad0e460fbd
vishu9656 <31932564+vishu9656@users.noreply.github.com>
2017-11-09 09:57:17 -0500

    Update and rename index.php to login.php

--- login.php
+++ login.php
@@ -1,23 +1,4 @@
-<?php
-session_start();
-
-$username = "user";
-$password = "password";
-if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']){
-	header("Location: success.php");
-}
-
-if(isset($_POST['username']) && isset($_POST['password'])){
-	if ($_POST['username']== $username && $_POST['password']== $password)
-	{
-		$_SESSION['loggedin'] = true;
-		header("Location: success.php");
-	}
-	
-}
-
-?>
-
+<?php include 'dbconfig.php';?>
 <html>
 <body>
 <form method="post" action="success.php">
